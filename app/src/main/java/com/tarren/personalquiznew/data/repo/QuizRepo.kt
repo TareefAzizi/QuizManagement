de80988eb0c1fc7a7f3ae9af63ad5aedd7ec703a
TareefIman <tareef.iman.azizi@gmail.com>
2023-11-24 21:56:53 +0800

    working upload csv

--- app/src/main/java/com/tarren/personalquiznew/data/repo/QuizRepo.kt
+++ app/src/main/java/com/tarren/personalquiznew/data/repo/QuizRepo.kt
@@ -1,8 +1,9 @@
 package com.tarren.personalquiznew.data.repo
 
+import android.net.Uri
 import com.tarren.personalquiznew.data.model.Quiz
 import java.io.InputStream
 
 interface QuizRepo {
-    suspend fun createQuiz(quiz: Quiz)
+    suspend fun createQuiz(quiz: Quiz, csvFileUri: Uri)
 }
--- app/src/main/java/com/tarren/personalquiznew/data/repo/QuizRepoImpl.kt
+++ app/src/main/java/com/tarren/personalquiznew/data/repo/QuizRepoImpl.kt
@@ -1,5 +1,6 @@
 package com.tarren.personalquiznew.data.repo
 
+import android.net.Uri
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
@@ -15,19 +16,32 @@ class QuizRepoImpl(
 
     private val quizzesCollection = firestore.collection("quiz")
 
-    override suspend fun createQuiz(quiz: Quiz) {
+    override suspend fun createQuiz(quiz: Quiz, csvFileUri: Uri) {
         try {
             // Generate a unique ID for the quiz
             val quizId = UUID.randomUUID().toString()
-            val quizWithCustomId = quiz.copy(quizId = quizId)
+
+            // Upload CSV file to Firebase Storage
+            val csvFileUrl = uploadCsvFile(csvFileUri, quizId)
+
+            // Create a new Quiz object with the CSV file URL and the custom ID
+            val quizWithCustomIdAndCsv = quiz.copy(quizId = quizId, csvFileUrl = csvFileUrl)
 
             // Set the document with the generated ID
-            quizzesCollection.document(quizId).set(quizWithCustomId.toHashMap()).await()
+            quizzesCollection.document(quizId).set(quizWithCustomIdAndCsv.toHashMap()).await()
+
+            // Set the document with the generated ID
+            quizzesCollection.document(quizId).set(quizWithCustomIdAndCsv.toHashMap()).await()
 
             Log.d("QuizRepo", "Quiz created with ID: $quizId")
         } catch (e: Exception) {
             Log.e("QuizRepo", "Error creating quiz: ${e.message}", e)
             throw e
         }
+    }
+    private suspend fun uploadCsvFile(fileUri: Uri, quizId: String): String {
+        val fileRef = storageReference.child("quizzes/$quizId.csv")
+        fileRef.putFile(fileUri).await()
+        return fileRef.downloadUrl.await().toString()
     }
 }
